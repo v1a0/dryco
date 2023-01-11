513c8982b55c00056951653186963278e35f7f69
v1a0 <contact@v1a0.dev>
2023-01-11 18:36:46 +0700

    Added decorators
    Added decorator_with_kwargs, method_not_allowed and few tests

---
+++ src/decorators/main.py
@@ -0,0 +1,79 @@
+from functools import wraps
+
+
+def decorator_with_kwargs(orig_decorator: callable):
+    """
+    Super-decorator to create decorator that can handle kwargs
+
+    IMPORTANT: First argument of your decorator hould always catch decorating function
+               and have default value None. It's allso aloow you to run your decoator
+               without kwargs as a regular one, so that's why all other kwargs shoud
+               have implemented default value
+
+    Example:
+    >>> import superpython as sp
+    >>>
+    >>>
+    >>> @sp.decorator_with_kwargs
+    >>> def add_greetings(original_func: callable = None, message=None):
+    >>>     'Print greeting message before function ran'
+    >>>
+    >>>     def func_with_greetings(*args, **kwargs):
+    >>>         if message is None:  # print "Hello, world!" if message isn't set
+    >>>             print("Hello, world!")
+    >>>         else:
+    >>>             print(message)
+    >>>         original_func(*args, **kwargs)  # call original function
+    >>>
+    >>>     return func_with_greetings
+    >>>
+    >>>
+    >>> @add_greetings
+    >>> def calculations_1():
+    >>>     print('1 + 1 =', 1 + 1)
+    >>>
+    >>>
+    >>> @add_greetings(message='Wow, math is cool!')
+    >>> def calculations_2():
+    >>>     print('2 + 2 =', 2 + 2)
+    >>>
+    >>>
+    >>> calculations_1()
+    >>> # Hello, world!
+    >>> # 1 + 1 = 2
+    >>> calculations_2()
+    >>> # Wow, math is cool!
+    >>> # 2 + 2 = 4
+
+    :param orig_decorator: Original decorator
+    :return: Decorator that can handle kwargs
+    """
+
+    @wraps(orig_decorator)
+    def patched_decorator(func: callable = None, *args, **kwargs):
+        @wraps(func)
+        def patched_decorator_with_kwargs(orig_func: callable):
+            return orig_decorator(orig_func, *args, **kwargs)
+
+        # calling without kwargs
+        if func:
+            return patched_decorator_with_kwargs(func)
+
+        # calling with kwargs
+        return patched_decorator_with_kwargs
+
+    return patched_decorator
+
+
+@decorator_with_kwargs
+def method_not_allowed(method: callable, exception: Exception = None):
+    @wraps(method)
+    def not_allowed_method(*args, **kwargs):
+        if exception is None:
+            raise Exception("Method not allowed")
+        raise exception
+
+    return not_allowed_method
+
+
+__all__ = ["decorator_with_kwargs", "method_not_allowed"]
---
+++ tests/decorators/main.py
@@ -0,0 +1,154 @@
+import unittest
+
+
+class TestDecorators(unittest.TestCase):
+
+    def test_decorator_with_kwargs__funcs(self):
+        from src.decorators import decorator_with_kwargs
+
+        @decorator_with_kwargs
+        def add_5_to_result(original_func: callable = None, adder=None):
+            def decorated_func(*args, **kwargs):
+                result: int = original_func(*args, **kwargs)
+
+                if adder is None:
+                    return result + 5
+                else:
+                    return result + adder
+
+            return decorated_func
+
+        @add_5_to_result
+        def sum_1_and_1():
+            return 1 + 1
+
+        @add_5_to_result(adder=4)
+        def sum_2_and_2():
+            return 2 + 2
+
+        @add_5_to_result
+        def sum_3_and_n(n: int):
+            return 3 + n
+
+        @add_5_to_result(adder=4)
+        def sum_4_and_n(n: int):
+            return 4 + n
+
+        @add_5_to_result
+        def sum_5_n_m(n: int, m: int):
+            return 5 + n + m
+
+        @add_5_to_result(adder=4)
+        def sum_6_n_m(n: int, m: int):
+            return 6 + n + m
+
+        # raises =================================================
+        with self.assertRaises(TypeError) as context:
+            @add_5_to_result(imposible_kwarg="Something insane!!!")
+            def sum_nothing():
+                pass
+
+        self.assertEqual(sum_1_and_1(), 1 + 1 + 5)
+
+        self.assertEqual(sum_2_and_2(), 2 + 2 + 4)
+
+        self.assertEqual(sum_3_and_n(1), 3 + 1 + 5)
+        self.assertEqual(sum_3_and_n(n=1), 3 + 1 + 5)
+
+        self.assertEqual(sum_4_and_n(1), 4 + 1 + 4)
+        self.assertEqual(sum_4_and_n(n=1), 4 + 1 + 4)
+
+        self.assertEqual(sum_5_n_m(1, 2), 5 + 1 + 2 + 5)
+        self.assertEqual(sum_5_n_m(1, m=2), 5 + 1 + 2 + 5)
+        self.assertEqual(sum_5_n_m(n=1, m=2), 5 + 1 + 2 + 5)
+
+        self.assertEqual(sum_6_n_m(1, 2), 6 + 1 + 2 + 4)
+        self.assertEqual(sum_6_n_m(1, m=2), 6 + 1 + 2 + 4)
+        self.assertEqual(sum_6_n_m(n=1, m=2), 6 + 1 + 2 + 4)
+
+        with self.assertRaises(TypeError) as context:
+            sum_6_n_m(imposible_kwarg="Something insane!!!")
+
+    def test_decorator_with_kwargs__methods(self):
+        from src.decorators import decorator_with_kwargs
+
+        @decorator_with_kwargs
+        def louder_n_times(original_func: callable = None, n=None):
+            def decorated_func(*args, **kwargs):
+                result: str = original_func(*args, **kwargs)
+
+                if n is None:
+                    return result
+                else:
+                    return result.replace('!', '!' * n)
+
+            return decorated_func
+
+        class Dog:
+            def __init__(self, size: int):
+                self.size = size
+
+            def bark(self):
+                return f"Bark{'!' * (self.size // 2)}"
+
+            @louder_n_times
+            def regular_bark(self):
+                return self.bark()
+
+            @louder_n_times(n=3)
+            def best_bark(self):
+                return self.bark()
+
+            # raises =================================================
+            with self.assertRaises(TypeError) as context:
+                @louder_n_times(imposible_kwarg="Something insane!!!")
+                def broken_bark(self):
+                    pass
+
+        a_dog = Dog(size=2)
+
+        self.assertEqual(a_dog.bark(), "Bark!")
+        self.assertEqual(a_dog.regular_bark(), a_dog.bark())
+        self.assertEqual(a_dog.best_bark(), "Bark!!!")
+
+    def test_method_not_allowed(self):
+        from src.decorators import method_not_allowed
+
+        class Dog:
+
+            @method_not_allowed
+            def meow(self):
+                pass
+
+            @method_not_allowed
+            def meow_1(self, arg):
+                pass
+
+            @method_not_allowed
+            def meow_2(self, arg, kwarg):
+                pass
+
+            @method_not_allowed(exception=ConnectionResetError(1))
+            def meow_3(self, arg, kwarg):
+                pass
+
+            @method_not_allowed(exception=ConnectionResetError(1))
+            def meow_4(self, arg, kwarg):
+                pass
+
+            @method_not_allowed(exception=ConnectionResetError(1))
+            def meow_5(self, arg, kwarg):
+                pass
+
+        a_dog = Dog()
+
+        self.assertRaises(Exception, a_dog.meow)
+        self.assertRaises(Exception, a_dog.meow_1)
+        self.assertRaises(Exception, a_dog.meow_2)
+        self.assertRaises(ConnectionResetError, a_dog.meow_3)
+        self.assertRaises(ConnectionResetError, a_dog.meow_4)
+        self.assertRaises(ConnectionResetError, a_dog.meow_5)
+
+
+if __name__ == '__main__':
+    unittest.main()
